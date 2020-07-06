b9db023bd74388fb08053e859686ca68e8c5c26a
Bolaji Olayinka <bolajiolayinka980@gmail.com>
2020-07-06 11:30:11 +0100

    first phase

--- draft.py
+++ draft.py
@@ -67,6 +67,7 @@ for my_campaigns in campaigns:
     except Exception as e:
         print(e)
 for each_campaign in campaign_list:
+    file_name=((each_campaign)['campaign_name'])
     product_name=((each_campaign)['campaign_name']) + '.csv'
     for shopify_item in shopify_reader_list:
         if (each_campaign)['campaign_name'] == (shopify_item['campaign_name']).lower():
@@ -74,63 +75,65 @@ for each_campaign in campaign_list:
             each_campaign['total_sales']=float(shopify_item['total_sales'])      
             each_campaign['cost']=float(shopify_item['cost']) 
             each_campaign['capital_roi']=((each_campaign['total_sales']-each_campaign['spend']-each_campaign['cost'])/(each_campaign['spend']+each_campaign['cost'])) * (each_campaign['net_quantity']/each_campaign['spend'])
-        with open(product_name, 'a', newline='') as f:
-            fields=list(each_campaign.keys())
-            #print(fields)
-            output=csv.DictWriter(f, fieldnames=fields) 
-            fileEmpty = os.stat(product_name).st_size == 0
-            if fileEmpty:
-                output.writeheader()
-            output.writerow(each_campaign)
-            #break
-        with open(product_name, newline='') as read_csv:
-            user_reader=csv.DictReader(read_csv)
-            user_reader_list=(list(user_reader))
-            print(user_reader_list)
-            # each_campaign_axis=[]
-            cpc_axis=[]
-            ctr_axis=[]
-            purchase_rate_axis=[]
-            capital_roi = []
-            stop_date=[]
-            date=[]
-            count = 0
-            for item in user_reader_list:
-                cpc_axis.append(float(item['calc_cpc']))
-                result_fb = float(item['landing_page_view'])
-                impression_fb = float(item['impressions'])
-                orders_shopify = float(item['net_quantity'])
-                stop_date.append(item['date_stop'])
-                ctr_axis.append(float(item['calc_ctr']))
-                purchase_rate_axis.append(orders_shopify/result_fb)
-                capital_roi.append(float(item['capital_roi']))
+    if each_campaign.get('net_quantity') == None:
+        continue
+
+    print((each_campaign)['campaign_name'])
+    with open(product_name, 'a', newline='') as f:
+        fields=list(each_campaign.keys())
+        #print(fields)
+        output=csv.DictWriter(f, fieldnames=fields) 
+        fileEmpty = os.stat(product_name).st_size == 0
+        if fileEmpty:
+            output.writeheader()
+        output.writerow(each_campaign)
+        #break
+    with open(product_name, newline='') as read_csv:
+        user_reader=csv.DictReader(read_csv)
+        user_reader_list=(list(user_reader))
+        # each_campaign_axis=[]
+        cpc_axis=[]
+        ctr_axis=[]
+        purchase_rate_axis=[]
+        capital_roi = []
+        stop_date=[]
+        date=[]
+        count = 0
+        for item in user_reader_list:
+            cpc_axis.append(float(item['calc_cpc']))
+            result_fb = float(item['landing_page_view'])
+            impression_fb = float(item['impressions'])
+            orders_shopify = float(item['net_quantity'])
+            stop_date.append(item['date_stop'])
+            ctr_axis.append(float(item['calc_ctr']))
+            purchase_rate_axis.append(orders_shopify/result_fb)
+            capital_roi.append(float(item['capital_roi']))
+        print(item)
+        plt.rc('xtick', labelsize=6)
+        plt.rc('ytick', labelsize=6)
+        plt.figure()
+        plt.subplot(221)
+        plt.xticks(rotation=45, ha="right")
+        plt.plot(stop_date,cpc_axis,'-o')
+        plt.title('Cpc')
+        plt.grid(True)    
+        plt.subplot(222)
+        plt.xticks(rotation=45, ha="right")
+        plt.plot(stop_date,ctr_axis,'-o')
+        plt.title('Ctr')
+        plt.grid(True)
+        plt.subplot(223)
+        plt.xticks(rotation=45, ha="right")
+        plt.plot(stop_date,purchase_rate_axis,'-o')
+        plt.title('Purchase Rate')
+        plt.grid(True)
+        plt.subplot(224)
+        plt.xticks(rotation=45, ha="right")
+        plt.plot(stop_date,capital_roi,'-o')
+        plt.title('Capital Roi')
+        plt.grid(True)
+        plt.subplots_adjust(top=0.92, bottom=0.2, left=0.15, right=0.95, hspace=1.0,wspace=0.5)
+        plt.savefig(f"{file_name}")
             
-            plt.rc('xtick', labelsize=6)
-            plt.rc('ytick', labelsize=6)
-            plt.figure()
-            plt.subplot(221)
-            plt.xticks(rotation=45)
-            plt.plot(cpc_axis,'-o')
-            plt.title('Cpc')
-            plt.grid(True)    
-            plt.subplot(222)
-            plt.xticks(rotation=45)
-            plt.plot(ctr_axis,'-o')
-            plt.title('Ctr')
-            plt.grid(True)
-            plt.subplot(223)
-            plt.xticks(rotation=45)
-            plt.plot(purchase_rate_axis,'-o')
-            plt.title('Purchase Rate')
-            plt.grid(True)
-            plt.subplot(224)
-            plt.xticks(rotation=45)
-            plt.plot(capital_roi,'-o')
-            plt.title('Capital Roi')
-            plt.grid(True)
-            plt.subplots_adjust(top=0.92, bottom=0.1, left=0.10, right=0.95, hspace=0.25,wspace=0.2)
-            plt.show()
-            
-        break
-    break
+
 
